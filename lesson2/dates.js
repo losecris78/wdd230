ae91d1ca7aa14bf925e3aac1ce274cee57f665d7
losecris78 <lop2202@byui.edu>
2022-04-30 21:45:24 -0300

    finalweek2 ready

--- lesson2/dates.js
+++
@@ -1,4 +0,0 @@
-let year = Date.prototipe.getFullYear();
-document.querySelector(".currentYear").innerHTML=`${year}`;
-
-document.querySelector("#currentTime").innerHTML =`Last updated ${document.LastModified}`;
--- lesson2/js/dates.js
+++ lesson2/js/dates.js
@@ -1,3 +1,4 @@
+
 let today = new Date();
 document.querySelector("#current_year").innerHTML = today.getFullYear();
 document.querySelector('#currentTime').innerHTML = today;
