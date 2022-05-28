8f83952dcf5499e0a96d08b2c04462b63b37d800
losecris78 <lop2202@byui.edu>
2022-05-28 20:09:58 -0300

    week4 in progress

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,9 +1,8 @@
 let today = new Date();
 document.querySelector("#year").innerHTML = today.getFullYear();
 document.querySelector('#date').innerHTML = today;
-
+document.getElementById("lastModified").innerHTML= today;
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
 }
-const x =document.getElementById("hamburgerBtn")
-x.onclick = toggleMenu
+document.getElementById("hamburgerBtn").addEventListener("click",toggleMenu);
