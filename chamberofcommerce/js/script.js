b6585e1c0e8f5f6f24e4a052241a507519003439
losecris78 <lop2202@byui.edu>
2022-07-02 12:35:19 -0300

    discovery 1

--- chamberofcommerce/js/script.js
+++ chamberofcommerce/js/script.js
@@ -1,8 +1,10 @@
 let today = new Date();
 let year= document.querySelector("#year").innerHTML = today.getFullYear();
-let date= document.querySelector('#date').innerHTML = today;
+
+const fullDate = new Intl.DateTimeFormat("en-UK",{dateStyle: "full"}).format(today);
+let date= document.querySelector('#date').innerHTML = fullDate;
 let lastdate= document.getElementById("lastModified").innerHTML= today;
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
 }
-document.getElementById("hamburgerBtn").addEventListener("click",toggleMenu);
+document.getElementById("#hamburgerBtn").addEventListener("click",toggleMenu);
