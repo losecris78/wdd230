9acf817ba6f2b1ef7af7d99e3e3849920eb47aa2
Use your GitHub username <Enter your email address>
2022-10-07 04:01:01 -0300

    week4.1

---
+++ chamber/scripts/script.js
@@ -0,0 +1,29 @@
+let date = new Date;
+let year = date.getFullYear();
+let dateFormat = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(date);
+document.querySelector("#updated").textContent = `Updated: ${document.lastModified}`;
+
+document.querySelector("#year1").innerText = year;
+//document.querySelector("#update").innerHTML= date;
+document.querySelector("#todayis").innerHTML=dateFormat;
+function toggleMenu(){
+    document.getElementById("pNav").classList.toggle("open");
+}
+let bread= document.getElementById("hamb_button")
+bread.onclick= toggleMenu;
+document.getElementById("hamb_button").classList.toggle("open");
+
+/*const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;*/
--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -2,3 +2,5 @@ let date = new Date;
 let year = date.getFullYear();
 document.querySelector("#year1").innerText = year;
 document.querySelector("#update").innerHTML= date;
+
+
