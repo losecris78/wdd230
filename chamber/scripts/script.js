c4f3c4a9a650a084f0f6c03277fa3110ebe3f525
Use your GitHub username <Enter your email address>
2022-10-21 14:33:51 -0300

    week6.1

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,13 +1,20 @@
+//Date variables to get the las update and the actual date//
 let date = new Date;
 let year = date.getFullYear();
 let dateFormat = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(date);
 let day = date.getDay()
-console.log(day);
+/*console.log(day);*/
+
+//Last modified function//
 document.querySelector("#updated").textContent = `Updated: ${document.lastModified}`;
+//Year display for copyright//
 
 document.querySelector("#year1").innerText = year;
-//document.querySelector("#update").innerHTML= date;
+
+//Date displayed on header//
 document.querySelector("#todayis").innerHTML=dateFormat;
+
+//Toggle Menu for Hamburguer button//
 function toggleMenu(){
 	document.getElementById("pNav").classList.toggle("open");
 	document.getElementById("hamb_button").classList.toggle("open");
@@ -15,21 +22,8 @@ function toggleMenu(){
 let bread= document.getElementById("hamb_button");
 bread.onclick= toggleMenu;
 
-
+//Announcement that will appear only on Monday and Tuesday//
 if (day === 1) {document.getElementById("announcement").innerHTML ="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."};
 if (day === 2) {document.getElementById("announcement").innerHTML ="🤝🏼 Come join us for the chamber meet and greet tomorrow,Wednesday at 7:00 p.m"};
 /*if (day === 6) {document.getElementById("announcement").innerHTML ="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."};*/
-/*const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;*/
+/*const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.*/
