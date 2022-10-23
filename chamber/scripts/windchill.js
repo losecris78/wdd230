f946491fd5c3111cdc3a46ccfac9dec1df55d658
Use your GitHub username <Enter your email address>
2022-10-22 21:55:06 -0300

    windchill2.1

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,15 +1,10 @@
-let t = document.getElementById("temp");
-let s = document.getElementById("speed");
-console.log(t);
-console.log(s);
-let ch = function getChill(t, s){
-    35.74+(0.6215*t)-(35.75*(s**0.16))+0.4275+(s**0.16);}
-document.getElementById("chill").innerHTML=ch;
+let t = document.querySelector("#temp");
+let s = document.querySelector("#speed");
+function getChill(x,y){
+    return 35.74 + (0.6215 * x) - (35.75 * (y ** 0.16)) + 0.4275 + (y ** 0.16);}
+let ch = getChill(40,5);
+document.getElementById("chill").innerHTML= Math.ceil(ch);
 
-/*let ch = function getChill()
-    t * s;
-};
-document.getElementById("chill").innerHTML(ch)*/
-    
+
 
 
