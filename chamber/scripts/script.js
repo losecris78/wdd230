cd926ed8c1122945637e4082226ef87cd703ea9e
Use your GitHub username <Enter your email address>
2022-10-30 00:54:51 -0300

    week7.inprocess

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -26,8 +26,25 @@ bread.onclick= toggleMenu;
 if (day === 1) {document.getElementById("announcement").innerHTML ="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."};
 if (day === 2) {document.getElementById("announcement").innerHTML ="🤝🏼 Come join us for the chamber meet and greet tomorrow,Wednesday at 7:00 p.m"};
 /*if (day === 6) {document.getElementById("announcement").innerHTML ="🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."};*/
-/*const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.*/
-/*let t =document.getElementById("temp");
-let s = document.getElementById("speed");
-let ch = function getChill(){(t*s)};
-document.getElementById("chill").innerHTML=ch;*/
+
+// Local Storage coding
+
+const todayDisplay = document.querySelector(".visitor");
+const visitsDisplay = document.querySelector("#numberVisits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
